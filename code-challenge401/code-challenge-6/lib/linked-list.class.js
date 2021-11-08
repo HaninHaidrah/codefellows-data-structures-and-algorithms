41f5610f13859a78c160ebfed901062305d38b33
HaninHaidrah <haneenhadey@gmail.com>
2021-11-08 08:36:01 +0200

    code6

---
+++ code-challenge401/code-challenge-6/lib/linked-list.class.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const Node = require("./node.class");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      return this;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = newNode;
+
+    return this;
+  }
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let currentValue = this.head;
+      while (currentValue.next) {
+        currentValue = currentValue.next;
+      }
+      currentValue.next = node;
+    }
+  }
+  insertBefore(old, neu) {
+    let currentNode = this.head;
+    if (currentNode.value === old) {
+      this.insert(neu);
+    } else {
+      let nextNode;
+      while (currentNode.value !== old) {
+        currentNode = currentNode.next;
+        nextNode = currentNode.next;
+      }
+      const node = new Node(old);
+      currentNode.value = neu;
+      currentNode.next = node;
+      node.next = nextNode;
+    }
+  }
+  insertAfter(old, neu) {
+    let currentNode = this.head;
+    let nextNode;
+    while (currentNode.value !== old) {
+      currentNode = currentNode.next;
+      nextNode = currentNode.next;
+    }
+    if (currentNode.next === null) {
+      return this.append(neu);
+    } else {
+      let newNode = new Node(neu);
+      currentNode.next = newNode;
+      newNode.next = nextNode;
+    }
+  }
+}
+
+
+module.exports=LinkedList;
