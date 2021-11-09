e1c44a4cd163e731559e6499e374ee513cc2b0ed
HaninHaidrah <haneenhadey@gmail.com>
2021-11-09 23:03:40 +0200

    code8

---
+++ code-challenge401/code-challenge8/lib/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+const LinkedList = require('../lib/linked-list.class');
+
+
+
+const data = new LinkedList();
+
+data.append(1);
+data.append(2);
+data.append(3);
+data.append(4);
+console.log(data)
+
+
+const data1= new LinkedList();
+data1.append(7)
+data1.append(10)
+
+const lists=new LinkedList();
+console.log(lists.zipLists(data,data1))
+
+
